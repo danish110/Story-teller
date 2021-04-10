1c54f27fb8575baaf78be6ae1c121e0e2ae82427
danish110 <71767126+danish110@users.noreply.github.com>
2021-04-10 20:01:08 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,183 @@
+onEvent("image111", "click", function( ) {
+  open("https://youtu.be/L8_ZwBGntBw");
+});
+onEvent("image2", "click", function( ) {
+  open("https://youtu.be/7nOpDRZ2o4Q");
+});
+onEvent("image3", "click", function( ) {
+  open("https://youtu.be/Kmqc84IEV9k");
+});
+onEvent("image4", "click", function( ) {
+  open("https://youtu.be/NfkLyC3VnJ8");
+});
+onEvent("image5", "click", function( ) {
+  open("https://youtu.be/Sv5OItL7eWE");
+});
+onEvent("image6", "click", function( ) {
+  open("https://youtu.be/aZqZzXfbOb8");
+});
+
+
+
+
+
+onEvent("image7", "click", function( ) {
+  open("https://youtu.be/NUCbL7canzY");
+});
+
+
+onEvent("image8", "click", function( ) {
+  open("https://youtu.be/gqwjKbcfNBY");
+});
+
+
+
+
+onEvent("image9", "click", function( ) {
+  open("https://youtu.be/OnMJZFnjGQY");
+});
+
+
+onEvent("image10", "click", function( ) {
+  open("https://youtu.be/We3zcDvCfOo");
+});
+onEvent("image11", "click", function( ) {
+  open("https://youtu.be/IgCbMMfKk4A");
+});
+onEvent("image12", "click", function( ) {
+  open("https://youtu.be/Ca8uoyXLDcA");
+});
+
+
+onEvent("image13", "click", function( ) {
+  open("https://youtu.be/XBNuXIML5AM");
+});
+onEvent("image14", "click", function( ) {
+  open("https://youtu.be/vTGaBaK_ElY");
+});
+onEvent("image15", "click", function( ) {
+  open("https://youtu.be/J10SnPvktKc");
+});
+onEvent("image16", "click", function( ) {
+  open("https://youtu.be/_n34Wqj4NpQ");
+});
+onEvent("image17", "click", function( ) {
+  open("https://youtu.be/3YiZB3z-T8o");
+});
+onEvent("image18", "click", function( ) {
+  open("https://youtu.be/6pAXwZhyIs4");
+});
+
+
+
+
+onEvent("image19", "click", function( ) {
+  open("https://youtu.be/nv8llcM5B_o");
+});
+onEvent("image20", "click", function( ) {
+  open("https://youtu.be/W0Y2mcIHOr4");
+});
+onEvent("image21", "click", function( ) {
+  open("https://youtu.be/3olM-9vcd4M");
+});
+onEvent("image22", "click", function( ) {
+  open("https://youtu.be/OuGp0wS0ta0");
+});
+onEvent("image23", "click", function( ) {
+  open("https://youtu.be/oqALxx163CA");
+});
+onEvent("image24", "click", function( ) {
+  open("https://youtu.be/UQtYDgVoxXU");
+});
+onEvent("image25", "click", function( ) {
+  open("https://youtu.be/zqOjo6X_u4M");
+});
+onEvent("image26", "click", function( ) {
+  open("https://youtu.be/kX68L_gXHK8");
+});
+onEvent("image27", "click", function( ) {
+  open("https://youtu.be/N9Cn6VaBGKw");
+});
+onEvent("image28", "click", function( ) {
+  open("https://youtu.be/1yfVM0re4Jc");
+});
+onEvent("image29", "click", function( ) {
+  open("https://youtu.be/yj5IkYAioSc");
+});
+onEvent("image30", "click", function( ) {
+  open("https://youtu.be/g6492posINc");
+});
+onEvent("button1", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen4");
+});onEvent("button5", "click", function( ) {
+  setScreen("screen5");
+});
+
+
+
+
+onEvent("button-2", "click", function( ) {
+  setScreen("homescreen");
+});
+onEvent("button-3", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("button-4", "click", function( ) {
+  setScreen("screen2");
+});onEvent("button-5", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button-6", "click", function( ) {
+  setScreen("homescreen");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
